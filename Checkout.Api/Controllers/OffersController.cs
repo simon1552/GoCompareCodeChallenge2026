12dc82e25918bddb4171e9bf058a35706bf1aec0
simon1552 <55548012+simon1552@users.noreply.github.com>
2026-02-04 04:31:48 +0000

    Add offer controllers

---
+++ Checkout.Api/Controllers/OffersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Checkout.Api.Controllers;
+
+[ApiController]
+[Route("offers")]
+public class OffersController : ControllerBase
+{
+        
+    /*
+     * Currently, our system only accommodates one type of offer — multi-buy discounts.
+     * However, we anticipate introducing a variety of offer types in the future, details of which are yet to be confirmed.
+     * **We encourage you to design your solution with flexibility in mind, allowing for easy integration of new offer types as they become available.**
+     */
+    
+    // I would add a create Offer endpoint and enable it
+    
+
+
+}

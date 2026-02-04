49a05e0b6d78cd920d83be51214f3ef75671d754
simon1552 <55548012+simon1552@users.noreply.github.com>
2026-02-04 04:11:30 +0000

    Fix controller test and add implementation and model for controller

---
+++ Checkout.Api.Domain/Models/CheckoutResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Checkout.Api.Domain.Models;
+
+public class CheckoutResponse
+{
+    public int Total { get; set; }
+}
--- Checkout.Api/Controllers/CheckoutController.cs
+++ Checkout.Api/Controllers/CheckoutController.cs
@@ -1,13 +1,28 @@
 ﻿using Checkout.Api.Domain.Models;
+using Checkout.Api.Domain.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Checkout.Api.Controllers;
 
 [ApiController]
 [Route("checkout")]
-public class CheckoutController : ControllerBase
+public class CheckoutController(ILogger<CheckoutController> logger, ICheckoutService checkoutService) : ControllerBase
 {
     [HttpPost]
-    public Task<IActionResult> PostCheckout([FromBody] CheckoutRequest request)
-        => throw new NotImplementedException();
+    [ProducesResponseType(typeof(CheckoutResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> PostCheckout([FromBody] CheckoutRequest request)
+    {
+        try
+        {
+            var total = await checkoutService.PriceAsync(request);
+            return Ok(new CheckoutResponse { Total = total });
+        }
+        catch (Exception ex)
+        {
+            // Would use logger to track errors
+            const string message = "Failed to get request from body";
+            logger.LogError(ex, message);
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
 }

596966ac24b7f1be95865667bf65bde060527c10
simon1552 <55548012+simon1552@users.noreply.github.com>
2026-02-04 04:01:21 +0000

    Refactored Checkout.Api as domain was not referencing project, restarted it in Rider, Add test for controllers not implemented yet

---
+++ Checkout.Api/Controllers/CheckoutController.cs
@@ -0,0 +1,13 @@
+﻿using Checkout.Api.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checkout.Api.Controllers;
+
+[ApiController]
+[Route("checkout")]
+public class CheckoutController : ControllerBase
+{
+    [HttpPost]
+    public Task<IActionResult> PostCheckout([FromBody] CheckoutRequest request)
+        => throw new NotImplementedException();
+}
---
+++ Checkout.Tests/CheckoutControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Checkout.Api.Controllers;
+using Checkout.Api.Domain.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq.AutoMock;
+
+namespace Checkout.Tests;
+
+public class CheckoutControllerTests
+{
+    private readonly AutoMocker _autoMocker = new();
+    private readonly Fixture _fixture = new();
+
+    private readonly CheckoutController _subject;
+
+    public CheckoutControllerTests()
+    {
+        _subject = _autoMocker.CreateInstance<CheckoutController>();
+    }
+
+    [Fact]
+    public async Task GivenValidRequest_WhenPostCheckout_ThenThrowsNotImplementedException()
+    {
+        //Given
+        var request = _fixture.Create<CheckoutRequest>();
+
+        //When //Then
+        await Assert.ThrowsAsync<NotImplementedException>(
+            () => _subject.PostCheckout(request));
+    }
+
+    
+}

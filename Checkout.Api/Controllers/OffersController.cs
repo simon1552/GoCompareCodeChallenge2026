2dc674192d0baaa4c6d7a7ef7dbf5c943b46957f
simon1552 <55548012+simon1552@users.noreply.github.com>
2026-02-04 04:34:35 +0000

    Add comments on what I would do if i had more time

--- Checkout.Api/Controllers/OffersController.cs
+++ Checkout.Api/Controllers/OffersController.cs
@@ -13,7 +13,7 @@ public class OffersController : ControllerBase
      * **We encourage you to design your solution with flexibility in mind, allowing for easy integration of new offer types as they become available.**
      */
     
-    // I would add a create Offer endpoint and enable it
+    // I would add a create Offer endpoint and enable endpoint which we can add new offers on and disable any current offers 
     
 
 

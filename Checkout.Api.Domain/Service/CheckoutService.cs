8bbd0669851e8ca36550dc5acfded4376e9986db
simon1552 <55548012+simon1552@users.noreply.github.com>
2026-02-04 00:24:08 +0000

    Inital commit red failing test

---
+++ Checkout.Api.Domain/Service/CheckoutService.cs
@@ -0,0 +1,15 @@
+﻿namespace Checkout.Api.Domain.Service;
+
+public class CheckoutService
+{
+
+    public void Scan(String item)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public int GetTotal()
+    {
+        return 0; 
+    }
+}
---
+++ Checkout.Tests/CheckoutServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Checkout.Api.Domain.Service;
+
+namespace Checkout.Tests;
+
+public class CheckoutServiceTests
+{
+    [Fact]
+    public void CheckoutReceivesEmptyStringReturnsZero()
+    {
+        //Arrange
+        var checkout = new CheckoutService();
+        
+        //Act
+        checkout.Scan("");
+        var results = checkout.GetTotal();
+
+        //Assert
+        Assert.Fail();
+
+    }
+}

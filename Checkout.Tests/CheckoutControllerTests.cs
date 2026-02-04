235adb0ab3967e91a894c1f03bf6f34bcfa7202d
simon1552 <55548012+simon1552@users.noreply.github.com>
2026-02-04 04:06:49 +0000

    Add failing controller test

--- Checkout.Tests/CheckoutControllerTests.cs
+++ Checkout.Tests/CheckoutControllerTests.cs
@@ -1,11 +1,12 @@
 ﻿using AutoFixture;
-using System;
-using System.Threading.Tasks;
-using Xunit;
+
+using System.Net;
 using Checkout.Api.Controllers;
 using Checkout.Api.Domain.Models;
+using Checkout.Api.Domain.Services.Interface;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Moq.AutoMock;
 
 namespace Checkout.Tests;
@@ -23,15 +24,40 @@ public class CheckoutControllerTests
     }
 
     [Fact]
-    public async Task GivenValidRequest_WhenPostCheckout_ThenThrowsNotImplementedException()
+    public async Task GivenValidRequest_WhenPostCheckout_ThenReturns200OK()
+    {
+        //Given
+        //Set up fixtures
+        var request = _fixture.Create<CheckoutRequest>();
+        var total = _fixture.Create<int>();
+
+        //Set up mocks
+        _autoMocker.GetMock<ICheckoutService>()
+            .Setup(service => service.PriceAsync(request))
+            .ReturnsAsync(total);
+
+        //When
+        var result = (OkObjectResult)await _subject.PostCheckout(request);
+
+        //Then
+        //FluentAssertion
+        result.StatusCode.Should().Be((int)HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task GivenValidRequestThrowsException_WhenPostCheckout_ThenReturns500InternalServerError()
     {
         //Given
         var request = _fixture.Create<CheckoutRequest>();
 
-        //When //Then
-        await Assert.ThrowsAsync<NotImplementedException>(
-            () => _subject.PostCheckout(request));
-    }
+        _autoMocker.GetMock<ICheckoutService>()
+            .Setup(service => service.PriceAsync(request))
+            .ThrowsAsync(new Exception("Failed to get request from body"));
 
-    
+        //When
+        var result = (ObjectResult)await _subject.PostCheckout(request);
+
+        //Then
+        result.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+    }
 }
